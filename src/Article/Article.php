2ff7a21265d3dc37327c72b1957eb94bb304d710
Manuel Antoniotti <info@manuelprojects.com>
2018-11-29 15:40:47 +0100

    Created Test

---
+++ src/Article/Article.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace LaravelDay\Article;
+
+
+final class Article{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $title;
+    /**
+     * @var string
+     */
+    private $body;
+    /**
+     * @var \DateTime
+     */
+    private $creationDate;
+
+
+    /**
+     * Article constructor.
+     * @param int $id
+     * @param string $title
+     * @param string $body
+     * @param \DateTime $creationDate
+     */
+    public function __construct(int $id, string $title, string $body, \DateTime $creationDate)
+    {
+
+        $this->id = $id;
+        $this->title = $title;
+        $this->body = $body;
+        $this->creationDate = $creationDate;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+  
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreationDate(): \DateTime
+    {
+        return $this->creationDate;
+    }
+
+
+}
